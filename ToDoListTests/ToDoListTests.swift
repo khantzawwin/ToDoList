3b5241fac2e80f75897ddac9fa1fd3d0da596ac0
Khant Zaw Win <khantzawwin@weeswares.com>
2023-09-30 10:28:14 +0800

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Khant Zaw Win on 30/9/23.
+//
+
+import XCTest
+
+final class ToDoListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(2*2, 4)
+    }
+
+}
